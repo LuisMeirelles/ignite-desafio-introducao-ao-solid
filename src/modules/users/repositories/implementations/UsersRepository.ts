75cc3a0d3c938938c46bfc4491805d35538481d2
Luis Meirelles <luis.gustavo@essentia.com.br>
2022-12-23 11:34:20 -0300

    finaliza o desafio

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -1,13 +1,13 @@
-import { User } from "../../model/User";
-import { IUsersRepository, ICreateUserDTO } from "../IUsersRepository";
+import { User } from '../../model/User';
+import { IUsersRepository, ICreateUserDTO } from '../IUsersRepository';
 
 class UsersRepository implements IUsersRepository {
-  private users: User[];
+  private static users: User[];
 
   private static INSTANCE: UsersRepository;
 
   private constructor() {
-    this.users = [];
+    UsersRepository.users = [];
   }
 
   public static getInstance(): UsersRepository {
@@ -19,23 +19,31 @@ class UsersRepository implements IUsersRepository {
   }
 
   create({ name, email }: ICreateUserDTO): User {
-    // Complete aqui
+    const user = new User();
+
+    Object.assign(user, { name, email });
+
+    UsersRepository.users.push(user);
+
+    return user;
   }
 
   findById(id: string): User | undefined {
-    // Complete aqui
+    return UsersRepository.users.find((user) => user.id === id);
   }
 
   findByEmail(email: string): User | undefined {
-    // Complete aqui
+    return UsersRepository.users.find((user) => user.email === email);
   }
 
   turnAdmin(receivedUser: User): User {
-    // Complete aqui
+    receivedUser.admin = true;
+
+    return receivedUser;
   }
 
   list(): User[] {
-    // Complete aqui
+    return UsersRepository.users;
   }
 }
 
--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -1,15 +1,21 @@
-import { User } from "../../model/User";
-import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { User } from '../../model/User';
+import { IUsersRepository } from '../../repositories/IUsersRepository';
 
-interface IRequest {
+interface IResponse {
   user_id: string;
 }
 
 class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
-  execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+  execute({ user_id }: IResponse): User[] {
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user.admin) {
+      throw new Error('Only admin users can list all users');
+    }
+
+    return this.usersRepository.list();
   }
 }
 
