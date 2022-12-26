2a5f1cd4d0496df6e6cb176d457e34a755803f5d
Luis Meirelles <luis.gustavo@essentia.com.br>
2022-12-26 12:25:29 -0300

    Finaliza a documentação da api com Swagger

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
-import express from "express";
+import express from 'express';
+import swaggerUI from 'swagger-ui-express';
+import yaml from 'yamljs';
 
-import { usersRoutes } from "./routes/users.routes";
+import { usersRoutes } from './routes/users.routes';
 
 const app = express();
 
+const swaggerDocument = yaml.load('./openapi.yaml');
+
 app.use(express.json());
 
-app.use("/users", usersRoutes);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
+app.use('/users', usersRoutes);
 
 export { app };
